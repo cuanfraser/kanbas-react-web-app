44127437ca805bd2da42fcf6296d421336a6b8dc
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-09-17 17:46:22 -0400

    Add basic editor fields

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -26,6 +26,66 @@ export default function AssignmentEditor() {
                         <input id='wd-group' value={'ASSIGNMENTS'} />
                     </td>
                 </tr>
+
+                <tr>
+                    <td align='right' valign='top'>
+                        <label htmlFor='wd-display-grade-as'>Display Grade as</label>
+                    </td>
+                    <td>
+                        <input id='wd-display-grade-as' value={'Percentage'} />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td align='right' valign='top'>
+                        <label htmlFor='wd-submission-type'>Submission Type</label>
+                    </td>
+                    <td>
+                        <input id='wd-submission-type' value={'Online'} />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td align='right' valign='top'>
+                        <label htmlFor='wd-text-entry'>Online Entry Options</label>
+                    </td>
+                    <td>
+                        <input id='wd-text-entry' value={'Online'} />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td align='right' valign='top'>
+                        <label htmlFor='wd-assign-to'>Assign to</label>
+                    </td>
+                    <td>
+                        <input id='wd-assign-to' value={'Everyone'} />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td align='right' valign='top'>
+                        <label htmlFor='wd-due-date'>Due</label>
+                    </td>
+                    <td>
+                        <input id='wd-due-date' type='date' value={'05/13/2024'} />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td align='right' valign='top'>
+                        <label htmlFor='wd-available-from'>Available from</label>
+                    </td>
+                    <td>
+                        <input id='wd-available-from' type='date' value={'05/06/2024'} />
+                    </td>
+                    <td align='right' valign='top'>
+                        <label htmlFor='wd-available-until'>Until</label>
+                    </td>
+                    <td>
+                        <input id='wd-available-until' type='date' value={'05/20/2024'} />
+                    </td>
+                </tr>
             </table>
         </div>
     );
