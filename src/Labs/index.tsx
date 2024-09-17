4fd86211418ed22c9020152bd4d1228358e5cc9b
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-09-17 18:16:30 -0400

    Add class and section

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,7 +7,7 @@ export default function Labs() {
     return (
         <div id='wd-labs'>
             <h1>Labs</h1>
-            Cuan Fraser
+            Cuan Fraser CS5610 20595
             <TOC />
             <Routes>
                 <Route path='/' element={<Navigate to='Lab1' />} />
