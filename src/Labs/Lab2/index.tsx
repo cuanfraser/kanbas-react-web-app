90f49aa169086f155de426f06f0ee5aaebe29924
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-10-01 17:00:13 -0400

    A2 up to 3.2, install react icons

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -2,6 +2,15 @@ import './index.css';
 import { ForegroundColors } from './ForegroundColors';
 import { BackgroundColors } from './BackgroundColors';
 import { Borders } from './Borders';
+import { Padding } from './Padding';
+import { Margins } from './Margins';
+import { Corners } from './Corners';
+import { Dimensions } from './Dimensions';
+import { Positions } from './Positions';
+import { Zindex } from './Zindex';
+import { Float } from './Float';
+import { GridLayout } from './GridLayout';
+import { Flex } from './Flex';
 export default function Lab2() {
     return (
         <div id='wd-lab2'>
@@ -61,7 +70,15 @@ export default function Lab2() {
             <ForegroundColors />
             <BackgroundColors />
             <Borders />
-            
+            <Padding />
+            <Margins />
+            <Corners />
+            <Dimensions />
+            <Positions />
+            <Zindex />
+            <Float />
+            <GridLayout />
+            <Flex />
         </div>
     );
 }
