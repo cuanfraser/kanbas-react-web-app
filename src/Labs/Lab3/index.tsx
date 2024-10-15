2ead4c1850b5d7a4b7008abc2e9582d2ba06524f
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-10-15 15:09:26 -0400

    A3 2.4 to 2.4.8

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,21 @@
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import ArrowFunctions from "./ArrowFunctions";
 import BooleanVariables from "./BooleanVariables";
 import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
 import ConditionalOutputInline from "./ConditionalOutputInline";
+import FilterFunction from "./FilterFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import ForLoops from "./ForLoops";
+import House from "./House";
 import IfElse from "./IfElse";
+import ImpliedReturn from "./ImpliedReturn";
+import JsonStringify from "./JsonStringify";
+import LegacyFunctions from "./LegacyFunctions";
+import MapFunction from "./MapFunction";
+import SimpleArrays from "./SimpleArrays";
+import TemplateLiterals from "./TemplateLiterals";
 import TernaryOperator from "./TernaryOperator";
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
@@ -17,6 +31,20 @@ export default function Lab3() {
       <TernaryOperator/>
       <ConditionalOutputIfElse/>
       <ConditionalOutputInline/>
+      <LegacyFunctions/>
+      <ArrowFunctions/>
+      <ImpliedReturn/>
+      <TemplateLiterals/>
+      <SimpleArrays/>
+      <ArrayIndexAndLength/>
+      <AddingAndRemovingToFromArrays/>
+      <ForLoops/>
+      <MapFunction/>
+      <FindFunction/>
+      <FindIndex/>
+      <FilterFunction/>
+      <JsonStringify/>
+      <House/>
     </div>
   );
 }
