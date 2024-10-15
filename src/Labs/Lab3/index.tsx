6d2687b3092957ca8c293ee4d05150758dcf0990
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-10-15 15:20:13 -0400

    Complete 2.4

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -4,10 +4,13 @@ import ArrowFunctions from "./ArrowFunctions";
 import BooleanVariables from "./BooleanVariables";
 import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
 import ConditionalOutputInline from "./ConditionalOutputInline";
+import Destructing from "./Destructing";
+import DestructingImports from "./DestructingImports";
 import FilterFunction from "./FilterFunction";
 import FindFunction from "./FindFunction";
 import FindIndex from "./FindIndex";
 import ForLoops from "./ForLoops";
+import FunctionDestructing from "./FunctionDestructing";
 import House from "./House";
 import IfElse from "./IfElse";
 import ImpliedReturn from "./ImpliedReturn";
@@ -15,8 +18,11 @@ import JsonStringify from "./JsonStringify";
 import LegacyFunctions from "./LegacyFunctions";
 import MapFunction from "./MapFunction";
 import SimpleArrays from "./SimpleArrays";
+import Spreading from "./Spreading";
 import TemplateLiterals from "./TemplateLiterals";
 import TernaryOperator from "./TernaryOperator";
+import TodoItem from "./todo/TodoItem";
+import TodoList from "./todo/TodoList";
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
 
@@ -45,6 +51,12 @@ export default function Lab3() {
       <FilterFunction/>
       <JsonStringify/>
       <House/>
+      <TodoItem/>
+      <TodoList/>
+      <Spreading/>
+      <Destructing/>
+      <FunctionDestructing/>
+      <DestructingImports/>
     </div>
   );
 }
