1ef64c12dd34b76364114513a9524dff6c24465d
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-10-30 19:52:55 -0400

    Complete 2.2

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,7 +1,19 @@
-import ClickEvent from "./ClickEvent";
+import ClickEvent from './ClickEvent';
+import EventObject from './EventObject';
+import PassingDataOnEvent from './PassingDataOnEvent';
+import PassingFunctions from './PassingFunctions';
 
 export default function Lab4() {
-  return (
-    <ClickEvent />
-  );
+    function sayHello() {
+        alert('Hello');
+    }
+
+    return (
+        <div>
+            <ClickEvent />
+            <PassingDataOnEvent />
+            <PassingFunctions theFunction={sayHello} />
+            <EventObject />
+        </div>
+    );
 }
