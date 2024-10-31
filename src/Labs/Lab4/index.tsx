714dd5374c0d2694ed61c36d8e0631d30f6788f2
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-10-31 18:55:40 -0400

    Complete 2.3

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,7 +1,15 @@
+import ArrayStateVariable from './ArrayStateVariable';
+import BooleanStateVariables from './BooleanStateVariables';
+import ChildStateComponent from './ChildStateComponent';
 import ClickEvent from './ClickEvent';
+import Counter from './Counter';
+import DateStateVariable from './DateStateVariables';
 import EventObject from './EventObject';
+import ObjectStateVariable from './ObjectStateVariables';
+import ParentStateComponent from './ParentStateComponent';
 import PassingDataOnEvent from './PassingDataOnEvent';
 import PassingFunctions from './PassingFunctions';
+import StringStateVariables from './StringStateVariables';
 
 export default function Lab4() {
     function sayHello() {
@@ -14,6 +22,14 @@ export default function Lab4() {
             <PassingDataOnEvent />
             <PassingFunctions theFunction={sayHello} />
             <EventObject />
+            <Counter />
+            <BooleanStateVariables />
+            <StringStateVariables />
+            <DateStateVariable />
+            <ObjectStateVariable />
+            <ArrayStateVariable />
+            <ParentStateComponent />
+
         </div>
     );
 }
