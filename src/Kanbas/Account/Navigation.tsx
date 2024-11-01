73aba9106d37a6e790969ad76c8e23d9ff80f0a4
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-10-31 20:59:44 -0400

    Complete 3.4.4

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,24 +1,36 @@
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 export default function AccountNavigation() {
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
     return (
         <div id='wd-account-navigation' className='wd list-group fs-5 rounded-0'>
-            <Link to={`/Kanbas/Account/Signin`} className='list-group-item active border border-0'>
-                Signin
-            </Link>
-            <br />
-            <Link
-                to={`/Kanbas/Account/Signup`}
-                className='list-group-item text-danger border border-0'
-            >
-                Signup
-            </Link>
-            <br />
-            <Link
-                to={`/Kanbas/Account/Profile`}
-                className='list-group-item text-danger border border-0'
-            >
-                Profile
-            </Link>
+            {!currentUser && (
+                <div>
+                    <Link
+                        to={`/Kanbas/Account/Signin`}
+                        className='list-group-item active border border-0'
+                    >
+                        Signin
+                    </Link>
+                    <br />
+                    <Link
+                        to={`/Kanbas/Account/Signup`}
+                        className='list-group-item text-danger border border-0'
+                    >
+                        Signup
+                    </Link>
+                    <br />
+                </div>
+            )}
+
+            {currentUser && (
+                <Link
+                    to={`/Kanbas/Account/Profile`}
+                    className='list-group-item text-danger border border-0'
+                >
+                    Profile
+                </Link>
+            )}
             <br />
         </div>
     );
