45ead40d271b25ba620199f1fd17f6878bb65046
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-10-31 20:12:44 -0400

    Add modules crud buttons

--- src/Kanbas/Courses/Modules/ModulesControls.tsx
+++ src/Kanbas/Courses/Modules/ModulesControls.tsx
@@ -1,7 +1,16 @@
 import { FaPlus } from 'react-icons/fa6';
 import GreenCheckmark from './GreenCheckmark';
 import CancelIcon from './CancelIcon';
-export default function ModulesControls() {
+import ModuleEditor from './ModuleEditor';
+export default function ModulesControls({
+    moduleName,
+    setModuleName,
+    addModule,
+}: {
+    moduleName: string;
+    setModuleName: (title: string) => void;
+    addModule: () => void;
+}) {
     return (
         <div id='wd-modules-controls' className='text-nowrap'>
             <button id='wd-collapse-all' className='btn btn-lg btn-secondary me-1 float-end'>
@@ -10,7 +19,12 @@ export default function ModulesControls() {
             <button id='wd-view-progress' className='btn btn-lg btn-secondary me-1 float-end'>
                 View Progress
             </button>
-            <button id='wd-add-module-btn' className='btn btn-lg btn-danger me-1 float-end'>
+            <button
+                id='wd-add-module-btn'
+                className='btn btn-lg btn-danger me-1 float-end'
+                data-bs-toggle='modal'
+                data-bs-target='#wd-add-module-dialog'
+            >
                 <FaPlus className='position-relative me-2' style={{ bottom: '1px' }} />
                 Module
             </button>
@@ -59,6 +73,12 @@ export default function ModulesControls() {
                     </li>
                 </ul>
             </div>
+            <ModuleEditor
+                dialogTitle='Add Module'
+                moduleName={moduleName}
+                setModuleName={setModuleName}
+                addModule={addModule}
+            />
         </div>
     );
 }
