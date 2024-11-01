529fa7146e322a664df0adba9d3fef89bcddcf9a
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-10-31 20:00:48 -0400

    Fix bug of missing courses in courses in 3.2

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -54,7 +54,7 @@ export default function Kanbas() {
                             />
                         }
                     />
-                    <Route path='/Courses/:cid/*' element={<Courses />} />
+                    <Route path='/Courses/:cid/*' element={<Courses courses={courses} />} />
                     <Route path='/Calendar' element={<h1>Calendar</h1>} />
                     <Route path='/Inbox' element={<h1>Inbox</h1>} />
                 </Routes>
