e6b2f23883086c41d119824195bd220b63bf515f
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-10-31 20:15:16 -0400

    Add course img prop rendering if found

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -56,7 +56,7 @@ export default function Dashboard({
                                     className='wd-dashboard-course-link text-decoration-none text-dark'
                                     to={`/Kanbas/Courses/${course._id}/Home`}
                                 >
-                                    <img src='/images/reactjs.jpg' width='100%' height={160} />
+                                    <img src={course.img ? course.img : '/images/reactjs.jpg'} width='100%' height={160} />
                                     <div className='card-body'>
                                         <h5 className='wd-dashboard-course-title card-title'>
                                             {course.name}
