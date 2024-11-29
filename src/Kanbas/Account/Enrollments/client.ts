17cac0d5d4badaa847072d61d4ee11b72fe32fbb
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-11-29 16:32:40 -0500

    Use axioswithcredentials

--- src/Kanbas/Account/Enrollments/client.ts
+++ src/Kanbas/Account/Enrollments/client.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
+const axiosWithCredentials = axios.create({ withCredentials: true });
 
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const ENROLLMENTS_API = `${REMOTE_SERVER}/api/enrollments`;
 
 export const deleteEnrollment = async (enrollmentId: string) => {
-    const response = await axios.delete(`${ENROLLMENTS_API}/${enrollmentId}`);
+    const response = await axiosWithCredentials.delete(`${ENROLLMENTS_API}/${enrollmentId}`);
     return response.data;
 };
 
