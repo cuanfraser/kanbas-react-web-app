b379370cd31bd432c2172955a879f8c463ffea86
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-11-30 17:30:56 -0500

    Add client for quizzes

---
+++ src/Kanbas/Courses/Quizzes/client.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+const axiosWithCredentials = axios.create({ withCredentials: true });
+
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+const QUIZZES_API = `${REMOTE_SERVER}/api/quizzes`;
+const COURSES_API = `${REMOTE_SERVER}/api/courses`;
+
+export const createQuiz = async (courseId: String, quiz: any) => {
+  const { data } = await axiosWithCredentials.post(`${COURSES_API}/${courseId}`, quiz);
+  return data;
+};
+
+export const findQuizzesForCourse = async (courseId: String) => {
+  const { data } = await axiosWithCredentials.get(`${COURSES_API}/${courseId}/quizzes`);
+  return data;
+};
+
+export const updateQuiz = async (quiz: any) => {
+  const { data } = await axiosWithCredentials.put(`${QUIZZES_API}/${quiz._id}`, quiz);
+  return data;
+};
+
+export const deleteQuiz = async (quizId: string) => {
+  const response = await axiosWithCredentials.delete(`${QUIZZES_API}/${quizId}`);
+  return response.data;
+};
