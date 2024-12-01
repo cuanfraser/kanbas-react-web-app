6aa83b960ab07fcad89385f8c84f100a3900b09e
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-11-30 19:10:18 -0500

    Implement basic quiz list

--- src/Kanbas/Courses/Quizzes/QuizzesList.tsx
+++ src/Kanbas/Courses/Quizzes/QuizzesList.tsx
@@ -1,9 +1,52 @@
+import { BsGripVertical } from 'react-icons/bs';
+import FacultyOnly from '../../FacultyOnly';
+import { IoEllipsisVertical } from 'react-icons/io5';
+import { FaPlus } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+
 export default function QuizzesList({ quizzes }: { quizzes: any[] }) {
+  const getAvailability = (quiz: any) => {
+    if (!quiz.available || !quiz.available_until) {
+      return '';
+    }
+
+    const currentDate = new Date();
+    const available = new Date(quiz.available);
+    const availableUntil = new Date(quiz.available_until);
+    if (currentDate > availableUntil) {
+      return 'Closed';
+    }
+    if (available < currentDate && currentDate < availableUntil) {
+      return 'Available';
+    }
+    if (currentDate < available) {
+      return `Not available until ${available}`;
+    }
+    return '';
+  };
+
   return (
-    <div id='wd-quizzes-list' className='list-group'>
+    <ul id='wd-quizzes-list' className='list-group'>
+      <li className='list-group-item d-flex align-items-center fs-5 p-3 ps-2 bg-secondary'>
+        <BsGripVertical className='me-2 fs-3' />
+        <div className='fs-3'>Quizzes</div>
+      </li>
+
       {quizzes.map((quiz) => (
-        <li className='list-group-item p-3 ps-1'></li>
+        <li key={quiz._id} className='list-group-item p-3 ps-3 ps-5 d-flex align-items-center'>
+          <div className='quiz-list-details'>
+            <div className='fs-4'>{quiz.title}</div>
+            <div className=''>
+              <span className='me-2'>{getAvailability(quiz)}</span>
+              <span className='me-2'>Due {quiz.due}</span>
+              <span className='me-2'>{quiz.points} Pts</span>
+              <span className='me-2'>TODO Num Questions</span>
+              <span className='me-2'>TODO Student Score</span>
+            </div>
+          </div>
+          <div className='quiz-list-controls'></div>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 }
--- src/Kanbas/Courses/Quizzes/index.tsx
+++ src/Kanbas/Courses/Quizzes/index.tsx
@@ -1,21 +1,28 @@
-import { useEffect } from "react";
-import { useParams } from "react-router";
-import QuizzesControls from "./QuizzesControls";
-import QuizzesList from "./QuizzesList";
-
+import { useEffect } from 'react';
+import { useParams } from 'react-router';
+import QuizzesControls from './QuizzesControls';
+import QuizzesList from './QuizzesList';
+import { findQuizzesForCourse } from './client';
+import { setQuizzes } from './reducer';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function Quizzes() {
+  const dispatch = useDispatch();
   const { cid } = useParams();
-  const quizzes: any[] = [];
+  const { quizzes } = useSelector((state: any) => state.quizzesReducer);
 
-  // useEffect(() => {
-
-  // }[cid])
+  useEffect(() => {
+    const fetchQuizzes = async (courseId: String) => {
+      const quizzes = await findQuizzesForCourse(courseId);
+      dispatch(setQuizzes(quizzes));
+    };
+    fetchQuizzes(cid as String);
+  }, [cid, dispatch]);
 
   return (
     <div id='wd-quizzes' className='text-nowrap'>
       <QuizzesControls />
       <QuizzesList quizzes={quizzes} />
     </div>
-  )
+  );
 }
