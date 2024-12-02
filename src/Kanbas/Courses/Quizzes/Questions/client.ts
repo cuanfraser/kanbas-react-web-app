5c233caab78200df97e1bdb1dc8b8bc9be09dea0
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-12-02 15:54:09 -0500

    Add question client and type

---
+++ src/Kanbas/Courses/Quizzes/Questions/client.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { Question } from './types';
+const axiosWithCredentials = axios.create({ withCredentials: true });
+
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+const QUESTIONS_API = `${REMOTE_SERVER}/api/questions`
+const QUIZZES_API = `${REMOTE_SERVER}/api/quizzes`;
+
+export const createQuestion = async (quizId: string, question: Question) => {
+  const { data } = await axiosWithCredentials.post(`${QUIZZES_API}/${quizId}/questions`, question);
+  return data;
+};
+
+export const findQuestionById = async (questionId: string) => {
+  const { data } = await axiosWithCredentials.get(`${QUESTIONS_API}/${questionId}`);
+  return data;
+};
+
+export const findQuestionsForQuiz = async (quizId: string) => {
+  const { data } = await axiosWithCredentials.get(`${QUIZZES_API}/${quizId}/questions`);
+  return data;
+};
+
+export const updateQuestion = async (question: Question) => {
+  const { data } = await axiosWithCredentials.put(`${QUESTIONS_API}/${question._id}`, question);
+  return data;
+};
+
+export const deleteQuestion = async (questionId: string) => {
+  const response = await axiosWithCredentials.delete(`${QUESTIONS_API}/${questionId}`);
+  return response.data;
+};
---
+++ src/Kanbas/Courses/Quizzes/Questions/types.ts
@@ -0,0 +1,9 @@
+export type Question = {
+  _id: string;
+  title: string;
+  points: number;
+  question: string;
+  type: string;
+  choices: string[];
+  answer: string;
+};
