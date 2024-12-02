c609eb2332b0b64db68168fff77a035da531bfc9
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-12-01 19:00:12 -0500

    Style quiz details editor

--- src/Kanbas/Courses/Quizzes/QuizEditor/QuizDetailsEditor.tsx
+++ src/Kanbas/Courses/Quizzes/QuizEditor/QuizDetailsEditor.tsx
@@ -35,156 +35,168 @@ export default function QuizDetailsEditor({
         onChange={(e) => setQuiz({ ...quiz, description: e.target.value })}
       />
 
-      <div id='quiz-type-group' className='mb-3'>
-        <label htmlFor='quiz-type-select' className='form-label'>
+      <div id='quiz-type-group' className='row mb-3'>
+        <label htmlFor='quiz-type-select' className='col-sm-2 col-form-label text-sm-end'>
           Quiz Type
         </label>
-        <select
-          id='quiz-type-select'
-          className='form-select'
-          value={quiz.type}
-          required={true}
-          onChange={(e) => setQuiz({ ...quiz, type: e.target.value })}>
-          <option value='GRADED'>Graded Quiz</option>
-          <option value='PRACTICE'>Practice Quiz</option>
-          <option value='GRADED_SURVEY'>Graded Survey</option>
-          <option value='UNGRADED_SURVEY'>Ungraded Survey</option>
-        </select>
+        <div className='col-sm-10'>
+          <select
+            id='quiz-type-select'
+            className='form-select'
+            value={quiz.type}
+            required={true}
+            onChange={(e) => setQuiz({ ...quiz, type: e.target.value })}>
+            <option value='GRADED'>Graded Quiz</option>
+            <option value='PRACTICE'>Practice Quiz</option>
+            <option value='GRADED_SURVEY'>Graded Survey</option>
+            <option value='UNGRADED_SURVEY'>Ungraded Survey</option>
+          </select>
+        </div>
       </div>
 
-      <div id='quiz-group-group' className='mb-3'>
-        <label htmlFor='quiz-group-select' className='form-label'>
+      <div id='quiz-group-group' className='row mb-3'>
+        <label htmlFor='quiz-group-select' className='col-sm-2 col-form-label text-sm-end'>
           Assignment Group
         </label>
-        <select
-          id='quiz-group-select'
-          className='form-select'
-          value={quiz.group}
-          required={true}
-          onChange={(e) => setQuiz({ ...quiz, group: e.target.value })}>
-          <option value='QUIZZES'>Quizzes</option>
-          <option value='EXAMS'>Exams</option>
-          <option value='ASSIGNMENTS'>Assignments</option>
-          <option value='PROJECT'>Project</option>
-        </select>
-      </div>
-
-      <div id='quiz-options-group' className='mb-4'>
-        <b>Options</b>
-
-        <div id='quiz-shuffle-group' className='mb-2'>
-          <input
-            id='quiz-shuffle-input'
-            className='form-check-input me-2'
-            type='checkbox'
-            checked={quiz.shuffle}
-            onChange={(e) => setQuiz({ ...quiz, shuffle: e.target.checked })}
-          />
-          <label htmlFor='quiz-shuffle-input' className='form-check-label'>
-            Shuffle Answers
-          </label>
-        </div>
-
-        <div id='quiz-time-group' className='mb-2'>
-          <input
-            id='quiz-time-checkbox'
-            className='form-check-input me-2'
-            type='checkbox'
-            checked={quiz.time > 0}
-            onChange={(e) => handleTimeCheckbox(e)}
-          />
-          <label htmlFor='quiz-time-checkbox' className='form-check-label'>
-            Time Limit
-          </label>
-
-          <input
-            id='quiz-time-input'
-            className='form-control'
-            type='number'
-            value={quiz.time}
+        <div className='col-sm-10'>
+          <select
+            id='quiz-group-select'
+            className='form-select'
+            value={quiz.group}
             required={true}
-            disabled={quiz.time === 0}
-            onChange={(e) => setQuiz({ ...quiz, time: e.target.valueAsNumber })}
-          />
-          <label htmlFor='quiz-time-input' className='form-check-label'>
-            Minutes
-          </label>
-        </div>
-
-        <div id='quiz-multiple-attempts-group' className='input-group p-2 border rounded mb-2'>
-          <input
-            id='quiz-multiple-attempts-checkbox'
-            className='form-check-input me-2'
-            type='checkbox'
-            checked={quiz.multiple_attempts}
-            onChange={(e) => setQuiz({ ...quiz, multiple_attempts: e.target.checked })}
-          />
-          <label htmlFor='quiz-multiple-attempts-checkbox' className='form-check-label'>
-            Allow Multiple Attempts
-          </label>
-        </div>
-
-        <div id='quiz-show-correct-group' className='input-group mb-2'>
-          <input
-            id='quiz-show-correct-input'
-            className='form-control'
-            type='text'
-            value={quiz.show_correct}
-            placeholder='Show Correct Answers'
-            onChange={(e) => setQuiz({ ...quiz, show_correct: e.target.value })}
-          />
-          <label htmlFor='quiz-show-correct-input' className='form-label'>
-            Show Correct Answers
-          </label>
-        </div>
-
-        <div id='quiz-one-question-group' className='input-group mb-2'>
-          <input
-            id='quiz-one-question-checkbox'
-            className='form-check-input me-2'
-            type='checkbox'
-            checked={quiz.one_question_at_a_time}
-            onChange={(e) => setQuiz({ ...quiz, one_question_at_a_time: e.target.checked })}
-          />
-          <label htmlFor='quiz-one-question-checkbox' className='form-check-label'>
-            One Question at a Time
-          </label>
-        </div>
-
-        <div id='quiz-webcam-group' className='input-group mb-2'>
-          <input
-            id='quiz-webcam-checkbox'
-            className='form-check-input me-2'
-            type='checkbox'
-            checked={quiz.webcam}
-            onChange={(e) => setQuiz({ ...quiz, webcam: e.target.checked })}
-          />
-          <label htmlFor='quiz-webcam-checkbox' className='form-check-label'>
-            Webcam Required
-          </label>
-        </div>
-
-        <div id='quiz-lock-questions-group' className='input-group mb-2'>
-          <input
-            id='quiz-lock-questions-checkbox'
-            className='form-check-input me-2'
-            type='checkbox'
-            checked={quiz.lock_after_answers}
-            onChange={(e) => setQuiz({ ...quiz, lock_after_answers: e.target.checked })}
-          />
-          <label htmlFor='quiz-lock-questions-checkbox' className='form-check-label'>
-            Lock Questions After Answering
-          </label>
+            onChange={(e) => setQuiz({ ...quiz, group: e.target.value })}>
+            <option value='QUIZZES'>Quizzes</option>
+            <option value='EXAMS'>Exams</option>
+            <option value='ASSIGNMENTS'>Assignments</option>
+            <option value='PROJECT'>Project</option>
+          </select>
         </div>
       </div>
 
-      <div id='quiz-assign-group' className='mb-3'>
-        <label htmlFor='quiz-assign-input-group' className='form-label'>
+      <div id='quiz-options-group' className='row mb-4'>
+        <div className='col-sm-2'></div>
+        <div className='col-sm-10'>
+          <p className='fw-bold'>Options</p>
+
+          <div id='quiz-shuffle-group' className='mb-2'>
+            <input
+              id='quiz-shuffle-input'
+              className='form-check-input me-2'
+              type='checkbox'
+              checked={quiz.shuffle}
+              onChange={(e) => setQuiz({ ...quiz, shuffle: e.target.checked })}
+            />
+            <label htmlFor='quiz-shuffle-input' className='form-check-label'>
+              Shuffle Answers
+            </label>
+          </div>
+
+          <div id='quiz-time-group' className='row align-items-center mb-2'>
+            <div className='col-auto '>
+              <input
+                id='quiz-time-checkbox'
+                className='form-check-input me-2'
+                type='checkbox'
+                checked={quiz.time > 0}
+                onChange={(e) => handleTimeCheckbox(e)}
+              />
+              <label htmlFor='quiz-time-checkbox' className='form-check-label'>
+                Time Limit
+              </label>
+            </div>
+
+            <div className='col-auto'>
+              <input
+                id='quiz-time-input'
+                className='form-control'
+                type='number'
+                value={quiz.time}
+                required={true}
+                disabled={quiz.time === 0}
+                onChange={(e) => setQuiz({ ...quiz, time: e.target.valueAsNumber })}
+              />
+            </div>
+            <label htmlFor='quiz-time-input' className='col-auto col-form-check-label'>
+              Minutes
+            </label>
+          </div>
+
+          <div id='quiz-multiple-attempts-group' className='mb-2'>
+            <input
+              id='quiz-multiple-attempts-checkbox'
+              className='form-check-input me-2'
+              type='checkbox'
+              checked={quiz.multiple_attempts}
+              onChange={(e) => setQuiz({ ...quiz, multiple_attempts: e.target.checked })}
+            />
+            <label htmlFor='quiz-multiple-attempts-checkbox' className='form-check-label'>
+              Allow Multiple Attempts
+            </label>
+          </div>
+
+          <div id='quiz-show-correct-group' className='row mb-2'>
+            <label htmlFor='quiz-show-correct-input' className='col-auto col-form-label'>
+              Show Correct Answers
+            </label>
+            <div className='col-auto'>
+              <input
+                id='quiz-show-correct-input'
+                className='form-control'
+                type='text'
+                value={quiz.show_correct}
+                onChange={(e) => setQuiz({ ...quiz, show_correct: e.target.value })}
+              />
+            </div>
+          </div>
+
+          <div id='quiz-one-question-group' className='mb-2'>
+            <input
+              id='quiz-one-question-checkbox'
+              className='form-check-input me-2'
+              type='checkbox'
+              checked={quiz.one_question_at_a_time}
+              onChange={(e) => setQuiz({ ...quiz, one_question_at_a_time: e.target.checked })}
+            />
+            <label htmlFor='quiz-one-question-checkbox' className='form-check-label'>
+              One Question at a Time
+            </label>
+          </div>
+
+          <div id='quiz-webcam-group' className='mb-2'>
+            <input
+              id='quiz-webcam-checkbox'
+              className='form-check-input me-2'
+              type='checkbox'
+              checked={quiz.webcam}
+              onChange={(e) => setQuiz({ ...quiz, webcam: e.target.checked })}
+            />
+            <label htmlFor='quiz-webcam-checkbox' className='form-check-label'>
+              Webcam Required
+            </label>
+          </div>
+
+          <div id='quiz-lock-questions-group' className='mb-2'>
+            <input
+              id='quiz-lock-questions-checkbox'
+              className='form-check-input me-2'
+              type='checkbox'
+              checked={quiz.lock_after_answers}
+              onChange={(e) => setQuiz({ ...quiz, lock_after_answers: e.target.checked })}
+            />
+            <label htmlFor='quiz-lock-questions-checkbox' className='form-check-label'>
+              Lock Questions After Answering
+            </label>
+          </div>
+        </div>
+      </div>
+
+      <div id='quiz-assign-group' className='row mb-3'>
+        <label htmlFor='quiz-assign-input-group' className='col-sm-2 col-form-label text-sm-end'>
           Assign
         </label>
-        <div id='quiz-assign-input-group' className='border rounded'>
+        <div id='quiz-assign-input-group' className='col-sm-10 border rounded p-3'>
           <div id='quiz-assign-due-group' className='mb-2'>
-            <label htmlFor='quiz-due-input' className='form-label'>
+            <label htmlFor='quiz-due-input' className='form-label fw-bold'>
               Due
             </label>
             <input
@@ -198,7 +210,7 @@ export default function QuizDetailsEditor({
           </div>
 
           <div id='quiz-assign-available-group' className='mb-2'>
-            <label htmlFor='quiz-available-input' className='form-label'>
+            <label htmlFor='quiz-available-input' className='form-label fw-bold'>
               Available from
             </label>
             <input
@@ -212,7 +224,7 @@ export default function QuizDetailsEditor({
           </div>
 
           <div id='quiz-assign-available-until-group' className='mb-2'>
-            <label htmlFor='quiz-available-until-input' className='form-label'>
+            <label htmlFor='quiz-available-until-input' className='form-label fw-bold'>
               Until
             </label>
             <input
