ae9f73c98ca5b42b73782c8b2b4692f3f1db32f6
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-12-03 18:58:52 -0500

    Add QuizAttemptReducer

---
+++ src/Kanbas/Courses/Quizzes/Attempts/reducer.ts
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { QuizAttempt } from './types';
+
+interface QuizAttemptState {
+  quizAttempt: QuizAttempt;
+}
+
+const initialState: QuizAttemptState = {
+  quizAttempt: {
+    _id: '',
+    quiz_id: '',
+    user_id: '',
+    answers: [],
+    score: 0,
+    started: new Date(),
+    submitted: false,
+  },
+};
+
+const quizAttemptSlice = createSlice({
+  name: 'quizAttempt',
+  initialState,
+  reducers: {
+    setAttempt: (state, action) => {
+      state.quizAttempt = action.payload;
+    },
+
+    addQuestionAttempt: (state, { payload: questionAttempt }) => {
+      const existingAnswerIndx = state.quizAttempt.answers.findIndex(
+        (curr) => curr.question_id === questionAttempt.question_id
+      );
+      if (existingAnswerIndx === -1) {
+        state.quizAttempt.answers[existingAnswerIndx] = questionAttempt;
+      } else {
+        state.quizAttempt.answers.push(questionAttempt);
+      }
+    },
+  },
+});
+export const { setAttempt, addQuestionAttempt } = quizAttemptSlice.actions;
+export default quizAttemptSlice.reducer;
--- src/Kanbas/Courses/Quizzes/Attempts/types.ts
+++ src/Kanbas/Courses/Quizzes/Attempts/types.ts
@@ -2,8 +2,13 @@ export type QuizAttempt = {
   _id: string;
   quiz_id: string;
   user_id: string;
-  answers: [{ question_id: string; answer: string }];
+  answers: QuestionAttempt[];
   score: number;
   started: Date;
   submitted: boolean;
+};
+
+export type QuestionAttempt = {
+  question_id: string;
+  answer: string;
 };
